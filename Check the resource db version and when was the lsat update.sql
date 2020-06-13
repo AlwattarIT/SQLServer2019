1bedb8337f7e095742a4a655c5cd694be8b34e48
Nazar Al-Wattar <65359110+AlwattarIT@users.noreply.github.com>
2020-06-13 17:20:24 -0400

    Delete Check the resource db version and when was the lsat update.sql

--- Check the resource db version and when was the lsat update.sql
+++
@@ -1,3 +0,0 @@
---How to read the version of the resource database and when was the last update
-SELECT SERVERPROPERTY('ResourceVersion') ResourceDatabaseVersion,
-SERVERPROPERTY('ResourceLastUpdateDateTime') ResourceDatabaseLastupdatedate;
